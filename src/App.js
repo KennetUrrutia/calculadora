8419d82dcc02f7a99d95cc1a2d4c3531d41a82dc
Kenneth De Leon <Kenneth@somosmangoverde.com>
2022-11-06 12:04:35 -0600

    my firts commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import './App.css';
-import freeCodeCampLogo from './imagenes/freecodecamp-logo.png';
 import Boton from './componentes/Boton';
 import Pantalla from './componentes/Pantalla';
 import BotonClear from './componentes/BotonClear';
@@ -24,12 +23,6 @@ function App() {
 
   return (
     <div className='App'>
-      <div className='freecodecamp-logo-contenedor'>
-        <img 
-          src={freeCodeCampLogo}
-          className='freecodecamp-logo'
-          alt='Logo de freeCodeCamp' />
-      </div>
       <div className='contenedor-calculadora'>
         <Pantalla input={input}/>
         <div className='fila'>
